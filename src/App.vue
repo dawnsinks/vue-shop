f9bc4833aa47c27e400f50a1994e31188c531e62
dawn sink <412066247@qq.com>
2022-04-24 16:29:56 +0800

    set route

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
+  <Header/>
   <router-view/>
+  <Footer/>
 </template>
 
 <script>
-
-
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+export default {
+  components: {Footer, Header}
+}
 </script>
 
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
-
+  {
+    'path': '/',
+    component: ()=> import('../views/Home')
+  },
+  {
+    'path': '/search',
+    component: ()=> import('../views/Search')
+  },
+  {
+    'path': '/login',
+    component: ()=> import('../views/Login')
+  },
+  {
+    'path': '/register',
+    component: ()=> import('../views/Register')
+  }
 ]
 
 const router = createRouter({
